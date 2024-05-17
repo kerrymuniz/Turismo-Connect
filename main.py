b87e31d69766dadff1eff135b197fef130c63178
gilguerra1 <111063426+gilguerra1@users.noreply.github.com>
2024-05-17 19:53:38 -0300

    Introdução de novas classes

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ crud_pontos_turisticos = crud_pontos_turisticos.PontosTuristicos()
 crud_usuarios = crud_usuarios.Usuarios()
 crud_estabelecimentos_comerciais = crud_estabelecimentos_comerciais.EstabelicimentoComerciais()
 
-#crud_pontos_turisticos.criar_arquivo_csv()
-#crud_pontos_turisticos.cadastrar_ponto_turistico(nome="Kerry", endereco="lkkkkkkkkkkk", descricao="Lugar aberto",horario_de_funcionamento= "Seg - Sex: 6:00 - 22:00", media_de_avaliacoes= "4.9" )
-#crud_pontos_turisticos.cadastrar_ponto_turistico(nome="Prainha Zs", endereco="BV", descricao="Lugar aberto",horario_de_funcionamento= "Seg - Sex: 6:00 - 22:00", media_de_avaliacoes= "4.9" )
-#crud_pontos_turisticos.editar_informacao_ponto_turistico(id_ponto_turistico= 1, campo_de_alteracao="Endereço", nova_informacao="Rua apolo")
-#crud_pontos_turisticos.deletar_ponto_turistico(id_ponto_turistico= 13)
-#crud_pontos_turisticos.listar_pontos_turisticos_cadastrados()
+crud_pontos_turisticos.criar_arquivo_csv()
+crud_pontos_turisticos.cadastrar_ponto_turistico(nome="Kerry", endereco="lkkkkkkkkkkk", descricao="Lugar aberto",horario_de_funcionamento= "Seg - Sex: 6:00 - 22:00", media_de_avaliacoes= "4.9" )
+crud_pontos_turisticos.cadastrar_ponto_turistico(nome="Prainha Zs", endereco="BV", descricao="Lugar aberto",horario_de_funcionamento= "Seg - Sex: 6:00 - 22:00", media_de_avaliacoes= "4.9" )
+crud_pontos_turisticos.editar_informacao_ponto_turistico(id_ponto_turistico= 1, campo_de_alteracao="Endereço", nova_informacao="Rua apolo")
+crud_pontos_turisticos.deletar_ponto_turistico(id_ponto_turistico= 13)
+crud_pontos_turisticos.listar_pontos_turisticos_cadastrados()
 
-#crud_usuarios.criar_arquivo_csv()
+crud_usuarios.criar_arquivo_csv()
 
-#crud_usuarios.cadatrar_usuario(nome="Gilvando", cpf= 39821938, idade= 12, email= "ggg@cesar.school", telefone= 321312, endereco="iwjiawhdihawd")
-#crud_usuarios.editar_informacao_usuario(id_usuario= 1, campo_de_alteracao= "Nome", nova_informacao= "João")
-#crud_usuarios.deletar_usuario(id_usuario= 1)
-#crud_usuarios.listar_usuarios_cadastrados()
+crud_usuarios.cadatrar_usuario(nome="Gilvando", cpf= 39821938, idade= 12, email= "ggg@cesar.school", telefone= 321312, endereco="iwjiawhdihawd")
+crud_usuarios.editar_informacao_usuario(id_usuario= 1, campo_de_alteracao= "Nome", nova_informacao= "João")
+crud_usuarios.deletar_usuario(id_usuario= 1)
+crud_usuarios.listar_usuarios_cadastrados()
 
-#crud_estabelecimentos_comerciais.criar_arquivo_csv()
-#crud_estabelecimentos_comerciais.cadastrar_novo_estabelecimento_comercial(nome="Bode do nó", endereco="BV", 
-#                                                                          descricao="Restaurante", horario_de_funcionamento= "Seg - Sex: 12-22",
-#                                                                          medio_de_avaliacoes="4.5")
-#crud_estabelecimentos_comerciais.editar_informacao_estabelicimento_comercial(id_estabelecimento_comercial= 1, campo_de_alteracao="Nome", nova_informacao="Shopping Recife")
+crud_estabelecimentos_comerciais.criar_arquivo_csv()
+crud_estabelecimentos_comerciais.cadastrar_novo_estabelecimento_comercial(nome="Bode do nó", endereco="BV", 
+                                                                          descricao="Restaurante", horario_de_funcionamento= "Seg - Sex: 12-22",
+                                                                          medio_de_avaliacoes="4.5")
+crud_estabelecimentos_comerciais.editar_informacao_estabelicimento_comercial(id_estabelecimento_comercial= 1, campo_de_alteracao="Nome", nova_informacao="Shopping Recife")
 crud_estabelecimentos_comerciais.deletar_estabelicimento_comerciais(id_estabelecimento_comercial= 1)
 crud_estabelecimentos_comerciais.listar_estabelicimento_comerciais_cadastrados()
